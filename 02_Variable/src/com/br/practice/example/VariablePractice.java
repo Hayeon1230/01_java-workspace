580224bd7b4114b8c032bbabdb8955bb161e3ea7
Hayeon1230 <153481558+Hayeon1230@users.noreply.github.com>
2023-12-12 17:41:56 +0900

    변수 실습문제 2,3
    사칙연산, 면적 둘레

--- 02_Variable/src/com/br/practice/example/VariablePractice.java
+++ 02_Variable/src/com/br/practice/example/VariablePractice.java
@@ -1,5 +1,25 @@
 package com.br.practice.example;
-
+import java.util.Scanner;
 public class VariablePractice {
-
+	public void method2() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("첫 번째 정수를 입력하세요 : ");
+		int num1 = sc.nextInt();
+		System.out.print("두 번째 정수를 입력하세요 : ");
+		int num2 = sc.nextInt();
+		System.out.println("\n더하기 결과 : " + (num1 + num2));
+		System.out.println("빼기 결과 : " + (num1 - num2));
+		System.out.println("곱하기 결과 : " + (num1 * num2));
+		System.out.println("나누기 몫 결과 : " + (num1 / num2));
+		
+		
+	}
+	public void method3() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("가로 : ");
+		double row = sc.nextDouble();
+		System.out.print("세로 : ");
+		double col = sc.nextDouble();
+		System.out.print("\n" + "면적 : " + (row * col) + "\n둘레 : " + ((row+col)*2));
+	}
 }
--- 02_Variable/src/com/br/practice/run/Run.java
+++ 02_Variable/src/com/br/practice/run/Run.java
@@ -1,5 +1,14 @@
 package com.br.practice.run;
-
+import com.br.practice.example.VariablePractice;
 public class Run {
-
+	public static void main(String[] args) {
+		
+		VariablePractice a = new VariablePractice();
+		a.method2();
+		System.out.print("\n");
+		a.method3();
+		
+		
+		
+	}
 }
