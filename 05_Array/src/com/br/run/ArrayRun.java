49e61930f64749c845a0216a9331b085dd9acdea
Hayeon1230 <153481558+Hayeon1230@users.noreply.github.com>
2023-12-20 17:41:02 +0900

    05_Array

---
+++ 05_Array/src/com/br/run/ArrayRun.java
@@ -0,0 +1,14 @@
+package com.br.run;
+
+import com.br.array.*;
+
+public class ArrayRun {
+
+	public static void main(String[] args) {
+		
+		A_Array a = new A_Array();
+		//a.method11();
+		a.homework();
+	}
+
+}
