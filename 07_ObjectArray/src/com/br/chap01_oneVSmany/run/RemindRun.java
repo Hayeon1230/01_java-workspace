adb378dc10840b34aae6dfff2fa857bb490169ef
Hayeon1230 <153481558+Hayeon1230@users.noreply.github.com>
2023-12-29 17:41:46 +0900

    07_ObjectArray

---
+++ 07_ObjectArray/src/com/br/chap01_oneVSmany/run/RemindRun.java
@@ -0,0 +1,56 @@
+package com.br.chap01_oneVSmany.run;
+
+import com.br.chap01_oneVSmany.model.vo.Book;
+
+public class RemindRun {
+
+	public static void main(String[] args) {
+		
+		Book bk1 = new Book();
+		bk1.setTitle("자바의정석");
+		bk1.setAuthor("강개똥");
+		// price, publisher 필드에는 JVM의 초기값(0, null)이 담겨있을 것
+		
+		Book bk2 = new Book("혼공자", "신용권", 30000, "한빛미디어");
+		
+		System.out.println(bk1.information());
+		System.out.println(bk2.information());
+		
+		System.out.println("=================");
+		
+		int[] arr = new int[3];
+		// arr[0] == type : int
+		// arr[1] == type : int
+		// arr[2] == type : int
+		
+		arr[0] = 10;
+		arr[1] = 15;
+		arr[2] = 20;
+		
+		Book[] books = new Book[3];
+		// books	== type : Book[]
+		// books[0] == type : Book
+		// books[1] == type : Book
+		// books[2] == type : Book
+		books[0] = new Book();
+		books[0].setTitle("파이썬");
+		books[0].setAuthor("홍길동");
+		
+		books[1] = bk2;// 주소값을 대입 (bk2의 주소가 books[1]에)
+		
+		books[2] = new Book("도커", "이말순", 40000, "길벗");
+		
+		System.out.println(books); // Book[] 타입
+		System.out.println(books[0]); // Book 타입
+		
+		for(int i=0; i<books.length; i++) {
+			System.out.println(books[i].information());
+		}
+		
+		
+		
+		
+				
+	}
+
+}
---
+++ 07_ObjectArray/src/com/br/chap02_objectArray/run/ProgramRun.java
@@ -0,0 +1,64 @@
+package com.br.chap02_objectArray.run;
+
+import java.util.Scanner;
+
+import com.br.chap02_objectArray.model.vo.Phone;
+import com.br.chap02_objectArray.controller.*;
+
+public class ProgramRun { // ObjectArrayRun 내용을 프로그램화 시킴
+
+	public static void main(String[] args) {
+
+		// 현재 판매중인 핸드폰들
+		Phone[] phones = new Phone[4];
+
+		phones[0] = new Phone("롤리팝", "엘지", 100000, null);
+		phones[1] = new Phone("아이폰", "애플", 1700000, "15pro");
+		phones[2] = new Phone("아이폰", "애플", 1200000, "12pro");
+		phones[3] = new Phone("갤럭시", "삼성", 1400000, "s23");
+
+		Scanner sc = new Scanner(System.in);
+		PhoneController pc = new PhoneController();
+		
+		while (true) { // 무한반복
+			System.out.println("\n=== 하연폰판매점 ===");
+			System.out.println("1. 판매점 내의 전체 폰 정보 조회하기");
+			System.out.println("2. 판매점 내의 핸드폰 평균가 조회하기");
+			System.out.println("3. 구매하고자하는 핸드폰의 가격 조회하기");
+			System.out.println("0. 프로그램 종료하기");
+
+			System.out.println("\n메뉴입력 : ");
+			int menu = sc.nextInt();
+			sc.nextLine();
+			if (menu == 1) {
+				pc.selectAllPhone(phones);
+			} else if (menu == 2) {
+				//double avgPrice =  pc.selectPhoneAvgPrice(phones);
+				//System.out.println("평균가격 : " + avgPrice + "원");
+				System.out.println("평균가격 : " + pc.selectPhoneAvgPrice(phones) + "원");
+				
+			} else if (menu == 3) {
+				System.out.println("구매하고자 하는 핸드폰 : ");
+				String str = sc.nextLine();
+				
+				int[] arr = pc.selectBuyPhonePrice(phones, str);
+				//{0, 100000, 200000, 0} / {0, 0, 0, 0}
+				for(int i=0; i<arr.length; i++) {
+					if(arr[i] != 0) {
+						System.out.println("구매하고자 하는 핸드폰의 가격은" + arr[i] + "원입니다.");
+					}
+				}
+				// 만일 검색된 핸드폰이 없을 경우 => xxx핸드폰은 판매하고 있지않습니다.
+				// 결과
+				
+			} else if (menu == 0) {
+				System.out.println("프로그램을 종료합니다. 이용해주셔서 감사합니다.");
+				break;
+			}else {
+				System.out.println("잘못 입력하셨습니다. 다시 입력해주세요.");
+			}
+		}
+
+	}
+
+}
